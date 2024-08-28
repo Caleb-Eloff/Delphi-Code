adb946bf7b0819cba69d90fe000423194ef33a05
Caleb-Eloff <eloffcj@outlook.com>
2024-08-28 11:29:05 +0200

    Completed 08 - SoftDrink_2

--- 2024/Chapter 8 Data/08 - SoftDrink_2/conSoftDrink.dfm
+++ 2024/Chapter 8 Data/08 - SoftDrink_2/conSoftDrink.dfm
@@ -1,6 +1,27 @@
 object dbmSoftDrink: TdbmSoftDrink
-  OldCreateOrder = False
   OnCreate = DataModuleCreate
   Height = 288
   Width = 360
+  object ADOConnection1: TADOConnection
+    ConnectionString = 
+      'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\elocal21\D' +
+      'esktop\delphi-code\2024\Chapter 8 Data\08 - SoftDrink_1\SoftDrin' +
+      'kDB.mdb;Persist Security Info=False;'
+    LoginPrompt = False
+    Mode = cmShareDenyNone
+    Provider = 'Microsoft.Jet.OLEDB.4.0'
+    Left = 144
+    Top = 128
+  end
+  object ADOTable1: TADOTable
+    Connection = ADOConnection1
+    CursorType = ctStatic
+    TableName = 'tblSoftDrink'
+    Left = 248
+    Top = 152
+  end
+  object DataSource1: TDataSource
+    Left = 256
+    Top = 96
+  end
 end
--- 2024/Chapter 8 Data/08 - SoftDrink_2/conSoftDrink.pas
+++ 2024/Chapter 8 Data/08 - SoftDrink_2/conSoftDrink.pas
@@ -3,15 +3,22 @@ unit conSoftDrink;
 interface
 
 uses
-  SysUtils, Classes;
+  SysUtils, Classes, ADODB, DB;
 
 type
   TdbmSoftDrink = class(TDataModule)
+    ADOConnection1: TADOConnection;
+    ADOTable1: TADOTable;
+    DataSource1: TDataSource;
     procedure DataModuleCreate(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
+    conSoftDrinkDB: TADOConnection;
+    tblSoftDrink: TADOTable;
+    dscSoftDrink: TDataSource;
+
   end;
 
 var
@@ -23,8 +30,26 @@ implementation
 
 procedure TdbmSoftDrink.DataModuleCreate(Sender: TObject);
 begin
-   // OnCreate Code
+  // OnCreate Code
 
+  conSoftDrinkDB := TADOConnection.Create(dbmSoftDrink);
+  tblSoftDrink := TADOTable.Create(dbmSoftDrink);
+  dscSoftDrink := TDataSource.Create(dbmSoftDrink);
+
+  conSoftDrinkDB.Close;
+  conSoftDrinkDB.ConnectionString :=
+    'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' +
+    'C:\Users\elocal21\Desktop\delphi-code\2024\Chapter 8 Data\08 - SoftDrink_2\SoftDrinkDB.mdb'
+    + ';Persist Security Info=False';
+  conSoftDrinkDB.LoginPrompt := False;
+  conSoftDrinkDB.Open;
+
+  tblSoftDrink.Connection := conSoftDrinkDB;
+  tblSoftDrink.TableName := 'tblSoftDrink';
+
+  dscSoftDrink.DataSet := tblSoftDrink;
+
+  tblSoftDrink.Open;
 
 end;
 
